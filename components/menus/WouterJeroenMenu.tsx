65b8e58684625e69e4144101ea5c35f6f80b9b59
Frank Hartman <frank.hartman@lightyear.one>
2022-05-19 20:23:39 +0200

    Add menu Wouter

--- components/menus/WouterJeroenMenu.tsx
+++ components/menus/WouterJeroenMenu.tsx
@@ -7,19 +7,49 @@ export function WouterEnJeroen() {
       <Menu.Divider />
       <Menu.Grid>
         <Menu.Item
-          title="Basil Smash"
-          quote="Please don't break your glass"
-          ingredients={["Gin", "Citroensap", "Suikerstroop"]}
+          title="TOPPERTJE"
+          quote="En een breezer ananas"
+          ingredients={["Vodka, vigor bebio"]}
         ></Menu.Item>
         <Menu.Item
-          title="Basil Smash"
-          quote="Please don't break your glass"
-          ingredients={["Gin", "Citroensap", "Suikerstroop"]}
+          title="MOJITO"
+          quote="Wollah, wat zei je over mijn JITO!?!?"
+          ingredients={["White Rum", "Spa Red", "Mint", "Lime", "Sugar"]}
         ></Menu.Item>
         <Menu.Item
-          title="Basil Smash"
-          quote="Please don't break your glass"
-          ingredients={["Gin", "Citroensap", "Suikerstroop"]}
+          title="LIMON FIZZ"
+          quote="Het leven is een Fizzje, je moet alleen jezelf ophangen"
+          ingredients={["Limoncello", "Prosecco"]}
+        ></Menu.Item>
+        <Menu.Item
+          title="TEQUILA SUNRISE"
+          quote="Ohh heerlijk"
+          ingredients={["Tequila", "Orange Juice", "Grenadine(Kemper)"]}
+        ></Menu.Item>
+        <Menu.Item
+          title="GIN TONIC"
+          quote="Ik had Gin tonic!"
+          ingredients={["Gin", "Tonic", "Fruits"]}
+        ></Menu.Item>
+        <Menu.Item
+          title="PINA COLADA"
+          quote="Liedje van K3 - altijd 5 reeten!"
+          ingredients={["Malibu", "Pineapple", "Juice", "Coconut Milk"]}
+        ></Menu.Item>
+        <Menu.Item
+          title="AQUA DE VALENCIA"
+          quote="Orange Juice, Cava, Vodka, Gin, Sugar"
+          ingredients={["Orange Juice", "Cava", "Vodka", "Gin", "Sugar"]}
+        ></Menu.Item>
+        <Menu.Item
+          title="WHISKY SOUR"
+          quote="Mij niet beffe nie!"
+          ingredients={["Whisky", "Lemon", "Sugar"]}
+        ></Menu.Item>
+        <Menu.Item
+          title="SEX ON THE BEACH"
+          quote="Overal zat zand!"
+          ingredients={["Vodka", "Peachtree", "Orange Juice", "Cranberry"]}
         ></Menu.Item>
       </Menu.Grid>
     </Menu>
